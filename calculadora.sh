1e60254fefc560fe0012f6b5dc42805057fea5ae
João Baldo <joaosbaldo@gmail.com>
2025-03-03 15:58:25 -0300

    Adiciona script shell para executar a calculadora

---
+++ calculadora.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+read -p "Digite o seu nome para iniciar " nome
+
+echo "Seja Bem vindo(a) a nossa calculadora $nome!" 
+
+#Aqui execeutamos a calculadora diretamente via script:
+python3 /home/jvbaldo/modulo1/Praticando/Modulo1Pratico.py
+
